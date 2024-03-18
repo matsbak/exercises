e24ccd99c97d41b3c905e862fd90b5bc1c1d873a
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-03-18 01:28:30 +0100

    Added library example with Spring Data JPA database access exercise

---
+++ IDATA2306/library-example-spring-data-jpa-db-access/src/main/java/no/ntnu/idata2306/exercise/example/LibraryApplication.java
@@ -0,0 +1,16 @@
+package no.ntnu.idata2306.exercise.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entrypoint class for the application.
+ */
+@SpringBootApplication
+public class LibraryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryApplication.class, args);
+  }
+
+}
